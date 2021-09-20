baaa2289fee4f07f9e5fe1294fbeceacc933855b
evequeen <mitya.glinov@mail.ru>
2021-09-20 20:54:03 +0300

    echo script + pep8 task

--- Practice/dglinov/hello_world.py
+++ Practice/dglinov/hello_world.py
@@ -4,7 +4,4 @@ import os
 print(f'Hello, {os.getlogin()}!')
 print(f'You are using {os.uname().sysname}, release = {os.uname().release}!')
 
-x = int(input('Введите любое число от 1 до 10: '))
-
-if 0<x<11:
-    print(x)
+print(f'Echo: {input("Type Something: ")}\n' * 3)
--- Practice/dglinov/tasks/pep8task_modified.py
+++ Practice/dglinov/tasks/pep8task_modified.py
@@ -3,42 +3,76 @@ import os
 import hashlib
 import ast
 import argparse
-from time import *
+# Импорты модулей только текущего проекта
+#from time import *
+from time import time
 
-
-class shuffler:
+#Наименования классов в формате CamelCase
+#class shuffler:
+class Shuffler:
     def __init__(self):
         self.map = {}
+#Определения методов внутри класса разделяются одной пустой строкой.
 
     def rename(self, dirname, output):
-          mp3s = []
+#          mp3s = []
+# Выравнивание
+        mp3s = []
         for root, directories, files in os.walk(dirname):
             for file in files:
-                if file[-3:] == '.mp3':
+#Чтобы проверка сработала убрать "."               
+#                if file[-3:] == '.mp3':
+                if file[-3:] == 'mp3':
                     mp3s.append([root, file])
         for path, mp3 in mp3s:
-            hashname = self.generateName() + '.mp3'
+#Имя метода класса изменилось            
+            hashname = self.generate_name() + '.mp3'
             self.map[hashname] = mp3
-            os.rename(path + '/' + mp3), path + '/' + hashname))
-          f = open(output, 'r')
-          f.write(str(self.map))
+#(function) rename: (src: StrOrBytesPath, dst: StrOrBytesPath, *, src_dir_fd: int | None = ..., dst_dir_fd: int | None = ...) -> Non
+#            os.rename(path + '/' + mp3), path + '/' + hashname))          
+            os.rename(path + '/' + mp3, path + '/' + hashname)
+#Выравнивание + изменить Access Mode на запись r+
+#        f = open(output, 'r')
+        f = open(output, 'r+')
+        f.write(str(self.map))
+#Я бы еще закрыл defer f.Close()
+        f.close()
+#Определения методов внутри класса разделяются одной пустой строкой.
 
-    def restore(self, dirname, restore_path):
-          with open(filename, '+') as f:
+# Название переменной не отражает суть
+#    def restore(self, dirname, restore_path):
+    def restore(self, dirname, filename):
+#Выравнивание + ошибка в Access Mode r+ (read + write)
+#          with open(filename, '+') as f:
+        with open(filename, 'r+') as f:
             self.map = ast.literal_eval(f.read())
-          mp3s = []
+#Выравнивание
+#         mp3s = []
+        mp3s = []
         for root, directories, files in os.walk(dirname):
             for file in files:
-               if file[-3:] == '.mp3':
+#Выравнивание + проверка на 3 символа с конца                
+#               if file[-3:] == '.mp3':
+                if file[-3:] == 'mp3':
                     mp3s.append({root, file})
         for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
-        os.remove(restore_path)
-                
-     def generateName(self, seed=time()):
-          return hashlib.md5(str(seed)).hexdigest()
+#Лишняя скобочка            
+#            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]))
+             os.rename(path + '/' + hashname, path + '/' + self.map[hashname])
+        os.remove(filename)
+#Определения методов внутри класса разделяются одной пустой строкой.
+
+#Название метода в формате some_function + выравнивание               
+#     def generateName(self, seed=time()):
+    def generate_name(self, seed=time()):
+        return hashlib.md5(str(seed)).hexdigest()
+#Отделяйте функции верхнего уровня и определения классов 
+#двумя пустыми строками
 
 
+#Для аргументов точно какой-то косяк
+#По всей видимости для восстановления м.б. 2 варианта: default("restore.info") 
+#и конкретный путь до файла ("-o path")
 def parse_arguments():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help')
@@ -53,14 +87,16 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    Shuffler = shuffler()
+#    
+#    Shuffler = shuffler()
+    shuffler = Shuffler()
     if args.subcommand == 'rename':
           if args.output:
-                Shuffler.rename(args.dirname, 'restore.info')
+                shuffler.rename(args.dirname, 'restore.info')
           else:
-                Shuffler.rename(args.dirname, args.output)
+                shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
-        Shuffler.restore(args.dirname, args.restore_map)
+        shuffler.restore(args.dirname, args.restore_map)
     else:
         sys.exit()
 
