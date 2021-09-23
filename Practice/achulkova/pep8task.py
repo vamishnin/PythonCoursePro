26e4c9a4a266eb91ba86dd60419321f38d822f76
Anastasiya Chulkova <a.chulkova@mfisoft.ru>
2021-09-23 22:08:30 +0300

    pep8task - added comments

--- Practice/achulkova/pep8task.py
+++ Practice/achulkova/pep8task.py
@@ -6,13 +6,13 @@ import argparse
 from time import *
 
 
-class Shuffler:
+class Shuffler: #наименование класса в формате CamelCase
 
     def __init__(self):
         self.map = {}
 
     def rename(self, dirname, output):
-        mp3s = []
+        mp3s = [] #неправильный отступ
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
@@ -20,24 +20,24 @@ class Shuffler:
         for path, mp3 in mp3s:
             hashname = self.generateName() + '.mp3'
             self.map[hashname] = mp3
-            os.rename(path + '/' + mp3, path + '/' + hashname)
-            f = open(output, 'r')
-            f.write(str(self.map))
+            os.rename(path + '/' + mp3, path + '/' + hashname) #лишние скобки
+            f = open(output, 'r') #неправильный отступ
+            f.write(str(self.map)) #неправильный отступ
 
     def restore(self, dirname, restore_path):
-        with open(restore_path, '+') as f:
+        with open(restore_path, '+') as f: #неправильный отступ, в функцию open передавался не существующий аргумент
             self.map = ast.literal_eval(f.read())
-            mp3s = []
+            mp3s = [] #неправильный отступ
         for root, directories, files in os.walk(dirname):
             for file in files:
                 if file[-3:] == '.mp3':
                     mp3s.append({root, file})
         for path, hashname in mp3s:
-            os.rename(path + '/' + hashname, path + '/' + self.map[hashname])
-            os.remove(restore_path)
+            os.rename(path + '/' + hashname, path + '/' + self.map[hashname]) #лишние скобки
+            os.remove(restore_path) #неправильный отступ
 
-    def generateName(self, seed=time()):
-        return hashlib.md5(str(seed)).hexdigest()
+    def generateName(self, seed=time()): #неправильный отступ
+        return hashlib.md5(str(seed)).hexdigest() #неправильный отступ
 
 
 def parse_arguments():
@@ -53,13 +53,13 @@ def parse_arguments():
     return args
 
 
-def main():
+def main(): # добавлена 2-я пустая строка между функциями
     args = parse_arguments()
-    shuffler = Shuffler()
+    shuffler = Shuffler() #наименование класса - с большой буквы, переменной - с маленькой
     if args.subcommand == 'rename':
-        if args.output:
+        if args.output: #неправильный отступ
             shuffler.rename(args.dirname, 'restore.info')
-        else:
+        else: #неправильный отступ
             shuffler.rename(args.dirname, args.output)
     elif args.subcommand == 'restore':
         shuffler.restore(args.dirname, args.restore_map)
