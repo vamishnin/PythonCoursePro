e9be3a770b1a7396dc023308ef20c1e3beb6989d
Elena Bessonova <you@example.com>
2021-09-27 10:44:06 +0300

    Practice 1 (echo, pep8), practice 2 (tasks 1, 2)

---
+++ Practice/ebessonova/print_max.py
@@ -0,0 +1,21 @@
+def max(a, b):
+    if(a > b):
+        print(f'max {a}')
+    elif(b > a):
+        print(f'max {b}')
+    else:
+        print('equal')
+
+max(6, 5)
+
+
+def max_r(a, b):
+    if(a > b):
+        return a
+    elif(b > a):
+        return b
+    else:
+        return 'equal'
+
+res = max_r(6, 6)
+print(res)
