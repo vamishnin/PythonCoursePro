31506217e0d7be6a64a8ccb14cdf49840ce56858
Elena Bessonova <you@example.com>
2021-09-27 13:37:06 +0300

    fix Practice 1, 2

--- Practice/ebessonova/print_max.py
+++ Practice/ebessonova/print_max.py
@@ -1,7 +1,7 @@
 def max(a, b):
-    if(a > b):
+    if a > b:
         print(f'max {a}')
-    elif(b > a):
+    elif b > a:
         print(f'max {b}')
     else:
         print('equal')
@@ -10,12 +10,12 @@ max(6, 5)
 
 
 def max_r(a, b):
-    if(a > b):
+    if a > b:
         return a
-    elif(b > a):
+    elif b > a:
         return b
     else:
-        return 'equal'
+        return None
 
 res = max_r(6, 6)
 print(res)
