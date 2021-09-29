79336dbc33af278b35703210c457723128e6152d
advigura <nastyakuzmicheva@yandex.ru>
2021-09-29 18:14:23 +0300

    max2numbers

---
+++ Practice/advigura/compare.py
@@ -0,0 +1,19 @@
+
+
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def printmax(a, b):
+    if a > b:
+        print(f'print max is {a}')
+    else:
+        print(f'print max is {b}')
+        
+var1 = input("Please enter first number ") 
+# print (var1)
+var2 = input("Please enter second number ") 
+print(f'returned max is {max(var1,var2)}')
+printmax(var1, var2)
