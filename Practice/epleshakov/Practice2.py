512818c8e210b1bcbbb8e5a95c0db8c97a341396
zxfreemn <zxfreemn@users.noreply.github.com>
2021-09-29 10:35:50 +0300

    Practice #2 Q1 Q2 (modified)

--- Practice/epleshakov/Practice2.py
+++ Practice/epleshakov/Practice2.py
@@ -1,34 +1,35 @@
+# Варианты с max()
 def print_max(a, b):
-    print(max(a, b))
+    print(f"(print_max)Biggest number is: {max(int(a), int(b))}")  #
 
 
 def is_max(a, b):
-    return max(a, b)
+    return max(int(a), int(b))
 
 
+# Варианты с if
 def print_max2(a, b):
-    if a > b:
-        print(a)
-    elif a < b:
-        print(b)
+    if int(a) > int(b):
+        print(f"(print_max2)Biggest number is: {a})")
+    elif int(a) < int(b):
+        print(f"(print_max2)Biggest number is: {b}")
     else:
-        print('Numbers is equal')
+        print('(print_max2)Numbers is equal')
 
 
 def is_max2(a, b):
-    if a < b:
-        return b
-    elif a > b:
+    if int(a) > int(b):
         return a
+    elif int(a) < int(b):
+        return b
     return None
 
 
 first = input('Inter first number: ')
 second = input('Inter second number: ')
 
-
 print_max(first, second)
 print_max2(first, second)
 
-print('MAX number is: ' + is_max(first, second))
-print('MAX number is: ' + is_max2(first, second))
+print(f'(is_max)MAX number is: {is_max(first, second)}')
+print(f'(is_max2)MAX number is: {is_max2(first, second)}')
