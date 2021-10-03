11833bde23e557b10acc79514f18e87977c798bd
OlgaLavrenteva <olga.a.lavrentieva@gmail.com>
2021-10-03 13:19:51 +0300

    (Olga Lavrenteva) Practice#4: another implementation for task #3

--- Practice/olavrenteva/practice4/task3.py
+++ Practice/olavrenteva/practice4/task3.py
@@ -1,15 +1,27 @@
-def sort_choice(array, start_index=0):
-    if len(array[start_index:]) <= 1:
+# def sort_choice(array, start_index=0):
+#     if len(array[start_index:]) <= 1:
+#         return
+#
+#     index_min = start_index
+#     for i in range(start_index+1, len(array)):
+#         if array[i] < array[index_min]:
+#             index_min = i
+#     if index_min != start_index:
+#         array[start_index], array[index_min] = array[index_min], array[start_index]
+#
+#     sort_choice(array, start_index+1)
+
+def sort_choice(array):
+    if len(array) <= 1:
         return
 
-    index_min = start_index
-    for i in range(start_index+1, len(array)):
-        if array[i] < array[index_min]:
-            index_min = i
-    if index_min != start_index:
-        array[start_index], array[index_min] = array[index_min], array[start_index]
-
-    sort_choice(array, start_index+1)
+    for i in range(len(array) - 1):
+        index_min = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[index_min]:
+                index_min = j
+        if index_min != i:
+            array[i], array[index_min] = array[index_min], array[i]
 
 
 array = [2, 1, 2, 4, 3, -5, 1, 10, 0, -20, -0.5]
