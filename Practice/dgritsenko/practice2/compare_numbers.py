6d3fbcf60a2e9a831626011bafeaba5123d42f2b
Danila Gritsenko <dmg-work@yandex.ru>
2021-10-03 21:04:10 +0300

    Практика №2

---
+++ Practice/dgritsenko/practice2/compare_numbers.py
@@ -0,0 +1,34 @@
+def print_larger_number(a, b):
+    """
+    Prints maximum of two input values
+
+    :param a: First value
+    :param b: Second value
+    """
+    if (a > b):
+        print(f"larger number is {a}")
+    elif (a < b):
+        print(f"larger number is {b}")
+    else:
+        print(f"a == b == {a}")
+
+def return_larger_number(a, b):
+    """
+    Return maximum of values val1 and val2.
+
+    :param a: First value
+    :param b: Second value
+    :return: maximum of a and b
+    """
+    if (a > b):
+        return a
+    else:
+        return b
+
+print_larger_number(3, 5)
+print_larger_number(3, 1)
+print_larger_number(3, 3)
+
+larger_n = return_larger_number(3,5)
+print(larger_n)
+
---
+++ Practice/dgritsenko/practice2/tanks.py
@@ -0,0 +1,53 @@
+DAMAGE_VALUE = 90
+
+class Tank:
+    """
+    Base tank class
+    """
+
+    __health = 100
+    __is_alive = True
+
+    def __init__(self,
+                 model = "T-34",
+                 weight_t = 30,
+                 width_mm = 3000,
+                 height_mm = 2405,
+                 length_mm = 5920):
+        self.__model = model
+        self.weight_t = weight_t
+        self.width_mm = 3000
+        self.height_mm = 2405
+        self.length_mm = 5920
+
+    def attack(self, other):
+        print(f"Tank {self.__model} attacks tank {other.getModel()}!")
+        other.decreaseHealth(DAMAGE_VALUE)
+
+    def moveToPoint(self, x, y):
+        print(f"Tank {self.__model} moves to point x = {x}, y = {y}!")
+
+    def getHealth(self):
+        return self.__health
+
+    def getModel(self):
+        return self.__model
+
+    def decreaseHealth(self, delta):
+        if (self.__is_alive):
+            self.__health -= delta
+            if (self.__health < 0):
+                self.__is_alive = False
+
+    def isAlive(self):
+        return self.__is_alive
+
+
+# Main
+t1 = Tank()
+t2 = Tank("PzKpfw IV", 20, 2900, 2650, 5600)
+
+print(f"tank {t2.getModel()} has health = {t2.getHealth()}")
+t1.attack(t2)
+print(f"tank {t2.getModel()} has health = {t2.getHealth()} after attack by {t1.getModel()}")
+
