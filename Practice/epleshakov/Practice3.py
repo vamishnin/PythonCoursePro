fc7cdaf4f3a928673bfd99f3e461754c0068edea
zxfreemn <zxfreemn@users.noreply.github.com>
2021-10-05 07:36:33 +0300

    Practice #3 Q1 Q2

--- Practice/epleshakov/Practice3.py
+++ Practice/epleshakov/Practice3.py
@@ -1,40 +1,46 @@
 '''
 Практика №3
-Q1 - input_to_num()
-Q2 - is_palindrome(str)
+Q1 - input_to_sum() сложение чисел
+Q2 - is_palindrome(str) Проверка слова или фразы на полиндромность
 '''
-def input_to_num():
-    temp = int()
+import re
+
+def input_to_sum():
+    temp = float()
     while True:
-        x = input('Enter number or "stop": ')
-        if x.isdigit():
-            print(f'Number is: {x}')
-            temp += int(x)
-            print(f'Sum of latest numbers: {temp}')
-        elif x.lower() == 'stop':
-            print('Stopped')
-            break
-        else:
-            print(f'Wrong value: {x}')
+            x = input('Enter number or "stop": ')
+            temp_x = re.split("\.|-", x)
+            temp_x = ''.join(temp_x)
+            if temp_x.isdigit():
+                temp += float(x)
+                if temp % 1 == 0:
+                    print(f'Sum of latest numbers: {int(temp)}')
+                else:
+                    print(f'Sum of latest numbers: {temp}')
+            elif x.lower() == 'stop':
+                print('Stopped')
+                break
+            else:
+                print(f'Wrong value: {x}')
 
 
-input_to_num()
+input_to_sum()
+
 
 def is_palindrome(a):
     temp_A = a.split()
     temp_A = ''.join(temp_A)
-    print(temp_A)
     temp_B = a[::-1].split()
     temp_B = ''.join(temp_B)
-    print(temp_B)
     if temp_A.lower() == temp_B.lower():
         return True
+    return False
 
 
 f = 'Топот'
 
-print(is_palindrome(f))
+print(f'{f=}, is Polyndrom?: {is_palindrome(f)}')
 
 f = 'Кит на море романтик'
 
-print(is_palindrome(f))
+print(f'{f=}, is Polyndrom?: {is_palindrome(f)}')
