58c2c5a298c69261491a6a477cb1af420ec1af20
Elena Bessonova <you@example.com>
2021-10-15 18:07:38 +0300

    fix Practice 6 task 2, paragraph iter. Practice 7 task 5

--- Practice/ebessonova/practice_6/suppl_task_1.py
+++ Practice/ebessonova/practice_6/suppl_task_1.py
@@ -1,17 +1,47 @@
 # Реализовать итератор, который бы "читал" заданный текст по параграфам. Символ параграфа задается отдельно
 
-def text_iter_by_paragraph(filename, paragraph_symbol):
-    text = list()
-    with open(filename, 'r') as f:
-        for i in f.read().split(paragraph_symbol):
-            text.append(i)
+class ParagraphIter:
 
-    return iter(text)
+    def __init__(self, file, paragraph_symbol):
+        self.__file = file
+        self.__paragraph_symbol = paragraph_symbol
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        buffer = ''
+        buffer_sent = False
+        while True:
+            sym = self.__file.read(1)
+
+            if buffer_sent:
+                buffer = ''
+            # на случай, если символ параграфа состоит из нескольких символов,
+            # находим конец предыдущего параграфа
+            if buffer[len(buffer) - len(self.__paragraph_symbol):len(buffer)] == self.__paragraph_symbol:
+                buffer_sent = True
+                # возврашаем тект параграфа без символа параграфа
+                if len(buffer[:len(buffer) - len(self.__paragraph_symbol)]) != 0:
+                    return buffer[:len(buffer) - len(self.__paragraph_symbol)]
+            else:
+                buffer_sent = False
+                buffer += sym
+
+            if not sym:
+                # вернем последний текст параграфа перед концом файла
+                if len(buffer) != 0:
+                    return buffer
+                raise StopIteration
 
 
-text_iter = text_iter_by_paragraph('text_P.txt', 'P\n')
-while True:
+with open('text_P.txt', 'r') as f:
+    p = ParagraphIter(f, 'Paragraph')
     try:
-        print(next(text_iter))
+        print(next(p))
+        print(next(p))
+        print(next(p))
+        print(next(p))
     except StopIteration:
-        break
+        print('Too much "next" command')
+
--- Practice/ebessonova/practice_7/task_5.py
+++ Practice/ebessonova/practice_7/task_5.py
@@ -4,20 +4,10 @@ import re
 with open('../../README.md', 'r') as f:
     git_text = f.read()
 
-s = re.compile(r'\"git .*\"')
-s1 = re.compile(r'\: git .*')
-res = re.findall(s, git_text)
-res1 = re.findall(s1, git_text)
+s = re.compile(r'"(git .*)"')
+s1 = re.compile(r': (git .*)')
 
-result_list = list()
+result_list = re.findall(s, git_text) + re.findall(s1, git_text)
 
-# удаляем лишние элементы из команд
-for el in res:
-    el = el[1:len(el) - 1]
-    result_list.append(el)
-
-for el in res1:
-    el = el[2:]
-    result_list.append(el)
-
-print(result_list)
+for i in result_list:
+    print(i)
