af991a4f051d94b8bfa741c6c85d4987afa99bb8
OlgaLavrenteva <olga.a.lavrentieva@gmail.com>
2021-10-16 19:57:46 +0300

    (Olga Lavrenteva) Practice#7: warm-up task1 - file opening and closing moved out of __next__ to make it once

--- Practice/olavrenteva/practice7/part1/task1.py
+++ Practice/olavrenteva/practice7/part1/task1.py
@@ -1,8 +1,8 @@
 class ReadParagraph:
     def __init__(self, filepath, sep='\n'):
-        self._filepath = filepath
         self._sep = sep
         self._position = 0
+        self._file = open(filepath, 'r')
 
     def __iter__(self):
         return self
@@ -10,20 +10,22 @@ class ReadParagraph:
     def __next__(self):
         paragraph = ''
 
-        with open(self._filepath, 'r') as file:
-            file.seek(self._position)
-            char = file.read(1)
+        self._file.seek(self._position)
+        char = self._file.read(1)
 
-            if not char:
-                raise StopIteration
+        if not char:
+            raise StopIteration
 
-            while char and (char != self._sep or char == self._sep and paragraph == ''):
-                if char != self._sep:
-                    paragraph = paragraph + char
-                char = file.read(1)
+        while char and (char != self._sep or char == self._sep and paragraph == ''):
+            if char != self._sep:
+                paragraph = paragraph + char
+            char = self._file.read(1)
 
-            self._position = file.tell()
-            return paragraph
+        self._position = self._file.tell()
+        return paragraph
+
+    def __del__(self):
+        self._file.close()
 
 
 num = 0
