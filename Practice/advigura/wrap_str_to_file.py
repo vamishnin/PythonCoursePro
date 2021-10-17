e20ccc0094f183e3e49fdf30da934554289c7592
advigura <nastyakuzmicheva@yandex.ru>
2021-10-17 23:48:02 +0300

    fix_1

--- Practice/advigura/wrap_str_to_file.py
+++ Practice/advigura/wrap_str_to_file.py
@@ -4,14 +4,14 @@ import os
 class WrapStrToFile: 
     def __init__(self): 
         # здесь инициализируется атрибут filepath, он содержит путь до файла-хранилища 
-        self.filepath = mktemp("tmp", "prefix")
+        self.__filepath = mktemp("tmp", "prefix")
 
     @property 
     def content(self): 
         # попытка чтения из файла, в случае успеха возвращаем содержимое 
         # в случае неудачи возвращаем 'File doesn't exist' 
         try:
-            f = open(self.filepath, 'r')
+            f = open(self.__filepath, 'r')
             file_content = f.read()
             f.close()
             return file_content
@@ -21,18 +21,18 @@ class WrapStrToFile:
     @content.setter 
     def content(self, value): 
         # попытка записи в файл указанного содержимого 
-        f = open(self.filepath, 'w')
+        f = open(self.__filepath, 'w')
         f.write(value)
         f.close()
 
     @content.deleter 
     def content(self): 
         # удаляем файл: os.remove(имя_файла) 
-        os.remove(self.filepath)
+        os.remove(self.__filepath)
     
     @property
     def filename(self):
-        return self.filepath
+        return self.__filepath
 
 wstf = WrapStrToFile()
 print(f'filename = {wstf.filename}') 
