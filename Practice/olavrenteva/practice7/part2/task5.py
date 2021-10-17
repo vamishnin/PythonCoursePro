bb1caf454d78de764402aa4105cb05828dd02b54
OlgaLavrenteva <olga.a.lavrentieva@gmail.com>
2021-10-17 21:43:43 +0300

    (Olga Lavrenteva) Practice#7: task #5 - attempt to fix finding of git commands

--- Practice/olavrenteva/practice7/part2/task5.py
+++ Practice/olavrenteva/practice7/part2/task5.py
@@ -1,7 +1,7 @@
 import re
 
 commands = []
-with open('../../../README.md', 'r') as file:
+with open('../../../README.md', 'r', encoding='UTF-8') as file:
     while line := file.readline():
         if command := re.findall('(git [^а-я"\n)]+("[а-яА-Я ]+")?)', line):
             commands.extend([c[0] for c in command])
