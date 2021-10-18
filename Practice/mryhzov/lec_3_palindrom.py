4b6d24ee9dba28c9e4c9493868df47ab84b7529b
Max Ryzhov <31587306+max-ryzhov@users.noreply.github.com>
2021-10-18 16:55:38 +0300

    палиндром - if / else в одну строку

--- Practice/mryhzov/lec_3_palindrom.py
+++ Practice/mryhzov/lec_3_palindrom.py
@@ -5,12 +5,8 @@ word = word.lower()    # все буквы к строчным
 def check_pal(w):
     len_str = len(w)
     half_len = len_str // 2
-    if len_str % 2 == 0:
-        half_1 = w[:half_len]    # от начала до середины
-        half_2 = w[half_len:]    # от середины до конца
-    else:
-        half_1 = w[:half_len]
-        half_2 = w[(half_len + 1):]
+    half_1 = w[:half_len]    # 1я половина в любом случае от начала до half_len
+    half_2 = w[half_len:] if len_str % 2 == 0 else w[(half_len + 1):]  # 2я половина срезается взависимости от условия
     return half_1 == half_2[::-1]
 
 
