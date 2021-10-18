0d708ca8c5bbc267cf439cb8105be696b98fa334
Ilya Kolchugin <ikolchugin@mfisoft.ru>
2021-10-18 08:40:50 +0300

    Lesson7: доработка re.

--- Practice/ikolchugin/Lessons/Lesson7/job5_regexp.py
+++ Practice/ikolchugin/Lessons/Lesson7/job5_regexp.py
@@ -1,14 +1,12 @@
 import re
-import itertools
 
-git_re = re.compile(r'(git\s([a-z])+(\s-(\w|(-\w+)+))?(\s\".*\"|\s(https|git).*?\.git|(?:\s\w+(?:\.\w+)?){0,2}))')
+git_re = re.compile(r'git\s[a-z]+(?:\s-(?:\w|(?:-\w+)+))?(?:\s\".*\"|\s(?:https|git).*?\.git|(?:\s\w+(?:\.\w+)?){0,2})')
 
 with open('../../../README.md', 'r', encoding='utf-8') as f:
     git_commands = []
     while line := f.readline():
         for res in re.findall(git_re, line):
-            # print(res)
-            git_commands.append(tuple(itertools.filterfalse(lambda x: not x.startswith('git '), res))[0])
+            git_commands.append(res)
 
 
 for i in sorted(set(git_commands)):
