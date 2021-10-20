25bd59926b3126b29b78900e4828195248f570ae
Max Ryzhov <31587306+max-ryzhov@users.noreply.github.com>
2021-10-20 10:34:26 +0300

    поправил 2е задание, добавил 3е задание 6й лекции

--- Practice/mryhzov/lec_6/lec6_2.py
+++ Practice/mryhzov/lec_6/lec6_2.py
@@ -1,10 +1,9 @@
-def multiplier(m=1, source=[1, 2, 3]):
-    mul_source = [i * m for i in source]    # списковое выражение
-    return mul_source
+def multiplier(m=1, source=(1, 2, 3)):      # source сделал кортежем
+    return [i * m for i in source]          # сразу вернуть списковое выражение
 
 
 mul_list1 = multiplier()
-mul_list2 = multiplier(5, [10, 20, 30])
+mul_list2 = multiplier(5, (10, 20, 30))
 print(mul_list1)
 print(mul_list2)
 
