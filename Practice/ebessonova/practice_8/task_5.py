b0b7c88269c79b28220f8cbbf2f2b3cadbfc87d6
Elena Bessonova <you@example.com>
2021-10-25 10:13:40 +0300

    fix Practice 8 tasks 1, 2, 4, 5

--- Practice/ebessonova/practice_8/task_5.py
+++ Practice/ebessonova/practice_8/task_5.py
@@ -1,35 +1,51 @@
 from Tasks.qrcode_task import qrcodelibrary
 import os
+import re
 
 
 class BankProps:
-    def __init__(self, sender_name, sender_surname, sender_middlename='', sm=100):
-        self.Code = '1'
-        self.Name = 'Someone'
-        self.PersonalAcc = '40703810642020000114'
-        self.BankName = 'Волго-Вятский  банк ПАО Сбербанк'
-        self.BIC = '042202603'
-        self.CorrespAcc = '30101810900000000603'
-        self.PayeeINN = '5260138955'
-        self.KPP = '526001001'
-        self.LastName = sender_surname
-        self.FirstName = sender_name
-        self.MiddleName = sender_middlename
-        self.Prpose = '001'
-        self.РауеrАddress = ''
-        self.Sm = sm
+    def __init__(self):
+        with open('../../../Tasks/qrcode_task/реквизиты.txt', 'r', encoding='utf-8') as f:
+            recs = f.read()
+            attr_name = re.compile(r'\'(.*=)(_.*)\|\'')
+            attr_name_ru = re.compile(r'\'(.*)\': \'(_.*)\'')
+            attr_value = re.compile(r'(.*): (.*)')
+            attr_name_list = re.findall(attr_name, recs)
+            attr_name_ru_list = re.findall(attr_name_ru, recs)
+            attr_value_list = re.findall(attr_value, recs)
+
+        attr_list = list()
+        for attr in attr_value_list:
+            for attr_code in attr_name_ru_list:
+
+                if attr[0].lower() == attr_code[0].lower():
+                    attr_list.append((attr[1], attr_code[1]))
+
+        attr_dict = list()
+        for attr in attr_name_list:
+            prop_name_added = False
+            for attr_code in attr_list:
+
+                if attr[1] == attr_code[1]:
+                    attr_dict.append((attr[0], attr_code[0]))
+                    prop_name_added = True
+            if not prop_name_added:
+                attr_dict.append((attr[0], ''))
+
+        self.__attr_dict = attr_dict
 
     def save_props(self, filename):
-        with open(filename, 'w') as f:
-            for prop_name, value in self.__dict__.items():
-                f.write(f'{prop_name}={value}|')
+        with open(filename, 'w') as f_qr:
+            for attr in self.__attr_dict:
+                f_qr.write(f'{attr[0]}{attr[1]}|')
 
 
-payment = BankProps('Ivan', 'Ivanov')
-payment.save_props('props.txt')
+if __name__ == '__main__':
+    payment = BankProps()
+    payment.save_props('props.txt')
 
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-path_to_qr = os.path.join(cur_dir, 'qr_output.jpg')
-with open('props.txt', 'r') as f:
-    qrcodelibrary.txt_to_qrcode(f.read().strip(), path_to_qr)
-    print(path_to_qr)
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    path_to_qr = os.path.join(cur_dir, 'qr_output.jpg')
+    with open('props.txt', 'r') as f:
+        qrcodelibrary.txt_to_qrcode(f.read().strip(), path_to_qr)
+        print(path_to_qr)
