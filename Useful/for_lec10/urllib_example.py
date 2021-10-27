2c9a6b76fb78bf41e5331a10f84503cb4be4dfb3
IlyaOrlov <orlov.soft.company@gmail.com>
2021-10-27 22:08:27 +0300

    Lec12

--- Useful/for_lec10/urllib_example.py
+++ Useful/for_lec10/urllib_example.py
@@ -5,5 +5,7 @@ import re
 req = request.Request('https://yandex.ru/pogoda/nizhny-novgorod?lat=56.326887&lon=44.005986')
 response = request.urlopen(req)
 web_page = response.read().decode()
-res = re.findall(r'<span class="temp__value temp__value_with-unit">([+|-]\d)+</span>', web_page)
+
+s = re.compile(r'Текущая температура</span><span class="temp__value temp__value_with-unit">([+|-]\d+)</span>')
+res = re.findall(s, web_page)
 print(res)
