0a188d5b32334d4eb3f9998c02bd4ad52e4a7360
Ilya Kolchugin <ikolchugin@mfisoft.ru>
2021-10-29 17:04:30 +0300

    Lesson10: job3 url grab with urllib

--- Practice/ikolchugin/Lessons/Lesson10/job3_urls.py
+++ Practice/ikolchugin/Lessons/Lesson10/job3_urls.py
@@ -1,28 +1,30 @@
 from multiprocessing.pool import ThreadPool
-import urllib
+from urllib import request
+from urllib.error import HTTPError
 import re
 
 
 def isalive(_link):
-    a=urllib.urlopen(_link)
-    return _link,a.g
+    try:
+        response = request.urlopen(_link)
+        code = response.status
+    except HTTPError as e:
+        code = e.code
+    return _link, code
+
 
 def main():
     uri = 'https://google.com'
-    a = urllib.urlopen(uri)
-
-def main1():
     link_re = re.compile(r'(?:url|href|src)=\"(.*?)\"')
-    uri = 'https://google.com'
 
     links = re.findall(link_re, request.urlopen(uri).read().decode())
 
-    print(links)
     with ThreadPool(len(links)) as p:
         results = p.map(isalive, (link if link.startswith('http') else uri + link for link in links))
 
     for res in results:
         print(res)
+
 
 if __name__ == '__main__':
     main()
