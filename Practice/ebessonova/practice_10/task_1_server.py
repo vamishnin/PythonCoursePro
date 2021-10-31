149990325e47199b001b6b8cbce6c8ec9fb9efa7
Elena Bessonova <you@example.com>
2021-10-31 18:53:48 +0300

    fix practice 10 task 1, practice 9 task 2

--- Practice/ebessonova/practice_10/task_1_server.py
+++ Practice/ebessonova/practice_10/task_1_server.py
@@ -3,15 +3,14 @@ import socket
 
 
 word_dict = {'word': 'слово', 'letter': 'буква', 'sentence': 'предложение'}
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host = '127.0.0.1'
-port = 12345
-s.bind((host, port))
-while True:
-    elem, addr = s.recvfrom(1024)
-    print(f'Server got data from client: {elem.decode()}')
-    try:
-        s.sendto(word_dict[elem.decode()].encode(), addr)
-    except KeyError:
-        s.sendto(f'Слово "{elem.decode()}" не найдено в словаре'.encode(), addr)
-s.close()
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    host = '127.0.0.1'
+    port = 12345
+    s.bind((host, port))
+    while True:
+        elem, addr = s.recvfrom(1024)
+        print(f'Server got data from client: {elem.decode()}')
+        try:
+            s.sendto(word_dict[elem.decode()].encode(), addr)
+        except KeyError:
+            s.sendto(f'Слово "{elem.decode()}" не найдено в словаре'.encode(), addr)
