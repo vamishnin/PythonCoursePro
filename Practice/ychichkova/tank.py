6373621a9518bfdd50bd570751b3c08ceea65fc9
Yulia <J.Chichkova@mfisoft.ru>
2021-11-01 15:29:37 +0300

    task_tank_lec_2 - fixed

--- Practice/ychichkova/tank.py
+++ Practice/ychichkova/tank.py
@@ -1,14 +1,15 @@
 class Tank:
     def __init__(self, model, weight, brunt, fire):
-        self.model = model
-        self.weight = weight
-        self.brunt = brunt
-        self.fire = fire
+        self.__model = model
+        self.__weight = weight
+        self.__brunt = brunt
+        self.__fire = fire
 
     def get_tank_info(self):
-        print(f'The tank is {self.model}, weight {self.weight} kg, brunt {self.brunt} units, '
-                  f'fire {self.fire} shot per min')
+        print(f'The tank is {self.__model}, weight {self.__weight} kg, brunt {self.__brunt} units, '
+                  f'fire {self.__fire} shot per min')
 
 
 tank1 = Tank('MC-1', 240, 215, 62)
 tank1.get_tank_info()
+
