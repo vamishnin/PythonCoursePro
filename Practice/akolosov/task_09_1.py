ba02139a1eadea6721c647e50810ef0d36f09ff4
Alexander Kolosov <kazh000@gmail.com>
2021-11-02 20:35:28 +0300

    akolosov: work with threads and proccesses in loop. make conclusion

--- Practice/akolosov/task_09_1.py
+++ Practice/akolosov/task_09_1.py
@@ -29,30 +29,30 @@ if __name__ == "__main__":
 
     start = time.perf_counter()
     threads = []
-    threads.append(threading.Thread(target=find_prime, args=(1000000,)))
-    threads[0].start()
-    threads.append(threading.Thread(target=find_prime, args=(2000000, 1000001)))
-    threads[1].start()
-    threads.append(threading.Thread(target=find_prime, args=(3000000, 2000001)))
-    threads[2].start()
+    for i in ((1000000,), (2000000, 1000001), (3000000, 2000001)):
+        threads.append(threading.Thread(target=find_prime, args=i))
+    for i in threads:
+        i.start()
     for i in threads:
         i.join()
     print(f'Threads total time: {time.perf_counter() - start}')
     # Without start() we get an exception 'RuntimeError' at join()
-    # Without join() the process waits for the threads but the time measure is wrong
+    # Without join() the main thread waits for the subthreads
+    # but all commands of main thread finish before the subthreads
+    # becase of that the time measure is wrong
     # ~22sec
 
     start = time.perf_counter()
     procs = []
-    procs.append(mp.Process(target=find_prime, args=(1000000,)))
-    procs[0].start()
-    procs.append(mp.Process(target=find_prime, args=(2000000, 1000001)))
-    procs[1].start()
-    procs.append(mp.Process(target=find_prime, args=(3000000, 2000001)))
-    procs[2].start()
+    for i in ((1000000,), (2000000, 1000001), (3000000, 2000001)):
+        procs.append(mp.Process(target=find_prime, args=i))
+    for i in procs:
+        i.start()
     for i in procs:
         i.join()
     print(f'Processes total time: {time.perf_counter() - start}')
     # Without start() we get an exception 'AssertionError' at join()
-    # Without join() the process waits for the subprocesses but the time measure is wrong
+    # Without join() the main process waits for the subprocesses
+    # but all commands of main process finish before the subprocess
+    # becase of that the time measure is wrong
     # ~8-9sec
--- Practice/akolosov/task_09_2.py
+++ Practice/akolosov/task_09_2.py
@@ -1,5 +1,6 @@
 import threading
 import multiprocessing as mp
+import time
 
 
 def my_add(*args):
@@ -12,23 +13,29 @@ def my_add(*args):
 
 if __name__ == "__main__":
     print("Threads:")
+    start = time.perf_counter()
     threads = []
-    threads.append(threading.Thread(target=my_add, args=(1, 2, 3, 4, 5, 6, 7)))
-    threads[0].start()
-    threads.append(threading.Thread(target=my_add, args=('1', '2', '3', '4', '5', '6', '7')))
-    threads[1].start()
-    threads.append(threading.Thread(target=my_add, args=([1], [2, 1], [3, 2], [4, 3])))
-    threads[2].start()
+    for i in ((1, 2, 3, 4, 5, 6, 7),
+              ('1', '2', '3', '4', '5', '6', '7'),
+              ([1], [2, 1], [3, 2], [4, 3])):
+        threads.append(threading.Thread(target=my_add, args=i))
+    for i in threads:
+        i.start()
     for i in threads:
         i.join()
+    print(f'Threads total time: {time.perf_counter() - start}')
 
     print("Processes:")
+    start = time.perf_counter()
     procs = []
-    procs.append(mp.Process(target=my_add, args=(1, 2, 3, 4, 5, 6, 7)))
-    procs[0].start()
-    procs.append(mp.Process(target=my_add, args=('1', '2', '3', '4', '5', '6', '7')))
-    procs[1].start()
-    procs.append(mp.Process(target=my_add, args=([1], [2, 1], [3, 2], [4, 3])))
-    procs[2].start()
+    for i in ((1, 2, 3, 4, 5, 6, 7),
+              ('1', '2', '3', '4', '5', '6', '7'),
+              ([1], [2, 1], [3, 2], [4, 3])):
+        procs.append(mp.Process(target=my_add, args=i))
+    for i in procs:
+        i.start()
     for i in procs:
         i.join()
+    print(f'Processes total time: {time.perf_counter() - start}')
+    # Threads is faster in ~10-15 times than processes
+    # because the 'cost' of creation subproccess much bigger than the function execution
