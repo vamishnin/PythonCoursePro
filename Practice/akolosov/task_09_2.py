6762e1508e6361d0ca233a98299a8f7a0029c484
Alexander Kolosov <kazh000@gmail.com>
2021-11-02 22:31:56 +0300

    akolosov: remove threads from task_09_2.py

--- Practice/akolosov/task_09_2.py
+++ Practice/akolosov/task_09_2.py
@@ -1,6 +1,4 @@
-import threading
 import multiprocessing as mp
-import time
 
 
 def my_add(*args):
@@ -12,21 +10,6 @@ def my_add(*args):
 
 
 if __name__ == "__main__":
-    print("Threads:")
-    start = time.perf_counter()
-    threads = []
-    for i in ((1, 2, 3, 4, 5, 6, 7),
-              ('1', '2', '3', '4', '5', '6', '7'),
-              ([1], [2, 1], [3, 2], [4, 3])):
-        threads.append(threading.Thread(target=my_add, args=i))
-    for i in threads:
-        i.start()
-    for i in threads:
-        i.join()
-    print(f'Threads total time: {time.perf_counter() - start}')
-
-    print("Processes:")
-    start = time.perf_counter()
     procs = []
     for i in ((1, 2, 3, 4, 5, 6, 7),
               ('1', '2', '3', '4', '5', '6', '7'),
@@ -36,6 +19,3 @@ if __name__ == "__main__":
         i.start()
     for i in procs:
         i.join()
-    print(f'Processes total time: {time.perf_counter() - start}')
-    # Threads is faster in ~10-15 times than processes
-    # because the 'cost' of creation subproccess much bigger than the function execution
