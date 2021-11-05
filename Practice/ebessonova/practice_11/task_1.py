738c9e1240d676c1a647eebcd51430a77188f7ab
Elena Bessonova <you@example.com>
2021-11-05 20:14:46 +0300

    fix Practice 11 task 1

--- Practice/ebessonova/practice_11/task_1.py
+++ Practice/ebessonova/practice_11/task_1.py
@@ -20,47 +20,8 @@ class SQLiteWrapper:
     def select(self, sql_req):
         cursor = self.__conn.execute(sql_req)
         with open('data.json', 'w') as f:
-            data_dict = dict()
-
             for row in cursor:
-                i = 0
-                data_dict.clear()
-
-                id_is_found = False
-                name_is_found = False
-                age_is_found = False
-                address_is_found = False
-                salary_is_found = False
-
-                while i < len(row):
-                    if sql_req.find(' id') > 0 and not id_is_found:
-                        data_dict.update({'id': row[i]})
-                        id_is_found = True
-                        i += 1
-                        continue
-                    if sql_req.find(' name') > 0 and not name_is_found:
-                        data_dict.update({'name': row[i]})
-                        name_is_found = True
-                        i += 1
-                        continue
-                    if sql_req.find(' age') > 0 and not age_is_found:
-                        data_dict.update({'age': row[i]})
-                        age_is_found = True
-                        i += 1
-                        continue
-                    if sql_req.find(' address') > 0 and not address_is_found:
-                        data_dict.update({'address': row[i]})
-                        address_is_found = True
-                        i += 1
-                        continue
-                    if sql_req.find(' salary') > 0 and not salary_is_found:
-                        data_dict.update({'salary': row[i]})
-                        salary_is_found = True
-                        i += 1
-                        continue
-                    i += 1
-
-                json.dump(data_dict, f)
+                json.dump(row, f)
 
 
 if __name__ == "__main__":
