de32daa13cb42ebe325388e3501a28fa0a80c316
substr <i.podlipsky@mfisoft.ru>
2021-11-06 16:42:38 +0300

    Исправил замечания. Изменил доступ name на protected

--- Practice/ipodlipsky/Homework/Practice5/man.py
+++ Practice/ipodlipsky/Homework/Practice5/man.py
@@ -1,22 +1,20 @@
-#Написать класс Man, который принимает имя в конструкторе.
-#Имеет метод solve_task, который просто выводит "I'm not ready yet".
-
-#Написать класс Pupil, у которого переопределен метод solve_task.
-#На этот раз он будет думать от 3 до 6 секунд (c помощью метода sleep библиотеки time и randint библиотеки random).
-
 import time
 import random
-
 
 class Man:
     def __init__(self, name):
         self._name = name
 
     def solve_task(self):
-        print("I'm not ready yet")
+        print(f'{self._name}: I\'m not ready yet"')
 
 class Pupil(Man):
-
     def solve_task(self):
         time.sleep(random.randint(3, 6))
-        print("I'm not ready yet")
+        print(f'{self._name}: I\'m not ready yet"')
+
+m = Man('Ivan')
+m.solve_task()
+
+p = Pupil('Kolya')
+p.solve_task()
