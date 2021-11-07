c73f2c435fb63c903f1ac1d652f35ff330526165
OlgaLavrenteva <olga.a.lavrentieva@gmail.com>
2021-11-07 19:00:50 +0300

    (Olga Lavrenteva) Practice#11 task#1 - changes according to review comments

--- Practice/olavrenteva/practice11/task1.py
+++ Practice/olavrenteva/practice11/task1.py
@@ -17,19 +17,28 @@ class SQLiteWrapper:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._conn.close()
 
-    def execute(self, query):
+    def execute(self, query, params=None):
+        if query.split()[0].lower() == 'select':
+            return self.select(query, params)
+
         try:
-            self._cur.execute(query)
+            if params:
+                self._cur.execute(query, params)
+            else:
+                self._cur.execute(query)
             self._conn.commit()
         except sqlite3.IntegrityError as exception:
             print(f'Exception {exception} has occurred while {query} execution')
 
-    def select(self, query):
+    def select(self, query, params=None):
         result = dict()
-        result["query"] = query
+        result["query"] = f"{query}, {params}" if params else query
         result["records"] = dict()
         item = 1
-        self.execute(query)
+        if params:
+            self._cur.execute(query, params)
+        else:
+            self._cur.execute(query)
         for row in self._cur.fetchall():
             result["records"][item] = dict(row)
             item += 1
@@ -69,26 +78,29 @@ if __name__ == '__main__':
         print(oct_elec_students_grades)
 
         date_exam1 = datetime.date(2021, 10, 1)
-        db.execute(f"INSERT INTO StudentGrades (StudentID, SubjectID, Date, Grade) "
-                   f"VALUES (5, 1, '{date_exam1}', 5)")
-        db.execute(f"INSERT INTO StudentGrades (StudentID, SubjectID, Date, Grade) "
-                   f"VALUES (5, 2, '{date_exam1}', 3)")
+        db.execute("INSERT INTO StudentGrades (StudentID, SubjectID, Date, Grade) "
+                   "VALUES (5, 1, (:date_exam1), 5)", {'date_exam1': date_exam1})
+        db.execute("INSERT INTO StudentGrades (StudentID, SubjectID, Date, Grade) "
+                   "VALUES (5, 2, (:date_exam1), 3)", {'date_exam1': date_exam1})
 
         # find Electric Power Engineering students grades from October
+        date_exam2 = datetime.date(2021, 10, 31)
         oct_elec_students_grades = db.select(
             "SELECT St.Name, St.Surname, C.Course, Sb.Subject, SG.Date, SG.Grade"
             " FROM Students AS St, Courses AS C, Subjects AS Sb, StudentGrades AS SG"
             " WHERE SG.StudentID = St.Id AND SG.SubjectID = Sb.Id AND St.CourseID = C.Id"
             " AND C.Course = 'Electric Power Engineering'"
-            " AND SG.Date BETWEEN '2021-10-01' AND '2021-10-31'")
+            " AND SG.Date BETWEEN (:date_exam1) AND (:date_exam2)",
+            {'date_exam1': date_exam1, 'date_exam2': date_exam2})
         print(oct_elec_students_grades)
 
         # find Electric Power Engineering students grades < 4 from October
-        oct_elec_students_grades = db.select(
+        oct_elec_students_grades = db.execute(
             "SELECT St.Name, St.Surname, C.Course, Sb.Subject, SG.Date, SG.Grade"
             " FROM Students AS St, Courses AS C, Subjects AS Sb, StudentGrades AS SG"
             " WHERE SG.StudentID = St.Id AND SG.SubjectID = Sb.Id AND St.CourseID = C.Id"
             " AND C.Course = 'Electric Power Engineering'"
-            " AND SG.Date BETWEEN '2021-10-01' AND '2021-10-31'"
-            " AND SG.Grade < 4")
+            " AND SG.Date BETWEEN (:date_exam1) AND (:date_exam2)"
+            " AND SG.Grade < 4",
+            {'date_exam1': date_exam1, 'date_exam2': date_exam2})
         print(oct_elec_students_grades)
